ba5b93eda92b35f0eeebd7e44fb563adff96cecb
shahinaaktershima <shima421914@gmail.com>
2023-10-18 11:36:48 +0600

    added-navbar

---
+++ src/component/Home/Navbar.jsx
@@ -0,0 +1,43 @@
+import { NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+    const links=<>
+    <li><NavLink to='/'>HOME</NavLink></li>
+    <li><NavLink to='/mycard'>MY CARDS</NavLink></li>
+    <li><NavLink to='/addproduct'>Add Product</NavLink></li>
+   
+    <li><NavLink to='/register'>Register</NavLink></li>
+    </>
+    return (
+        <div className="navbar bg-base-100 container mx-auto my-5">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+       {
+        links
+       }
+      </ul>
+    </div>
+<div className="flex items-center">  
+    <img className="h-[100px] w-[100px] rounded-full" src="https://i.postimg.cc/GhMh8frX/pexels-kaboompics-com-6335.jpg" alt="" />
+      <a className="btn btn-ghost normal-case text-xl">Entertainment and Media</a></div>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      {
+        links
+      }
+    </ul>
+  </div>
+  <div className="navbar-end">
+ <button className="btn"> <NavLink to='/login'>Login</NavLink></button>
+  </div>
+</div>
+    );
+};
+
+export default Navbar;
